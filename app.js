c1909660594fc6bd6309a1ea97a44ff6b637f770
José Antonio Escudero García <jaeg90@gmail.com>
2023-04-01 11:36:03 +0200

    In progress

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  const filePath = path.join(__dirname, "views", "home.html");
+  console.log(filePath);
+  res.sendFile(filePath);
+});
+
+app.get("/about", (req, res, next) => {
+  const filePath = path.join(__dirname, "views", "about.html");
+  console.log(filePath);
+  res.sendFile(filePath);
+});
+app.get("/works", (req, res, next) => {
+  const filePath = path.join(__dirname, "views", "works.html");
+  console.log(filePath);
+  res.sendFile(filePath);
+});
+app.get("/gallery", (req, res, next) => {
+  const filePath = path.join(__dirname, "views", "gallery.html");
+  console.log(filePath);
+  res.sendFile(filePath);
+});
+
+const port = 3000;
+app.listen(port, () => {});
